8801fd65df8513dbe4625ff2e381ed27f5af6032
Bread <luphoria@innocent.com>
2022-05-23 00:38:46 -0500

    make logs look better

--- src/log.ts
+++ src/log.ts
@@ -1,25 +1,42 @@
-import config from "./config"
+import config from "./config";
 import { MatrixClient } from "matrix-bot-sdk";
 import { db } from "./lookupUser";
 
 export const log = (log: {}, verbose: boolean, client: MatrixClient) => {
-    if(!db.logto) return false;
-    console.log("= = = = = = = = = = = = = = = =");
-    let toLog = "- - -";
-    if(verbose) {
-        if(config.verbose) {
-            Object.keys(log).forEach(name => {
-                toLog += name + ": " + log[name] + "\n";
-            });
-            toLog += "- - -";
-            client.sendNotice(db.logto,toLog);
+  if (!db.logto) return false;
+  console.log("= = = = = = = = = = = = = = = =");
+  let toLog = "- - -\n\n";
+  if (verbose) {
+    if (config.verbose) {
+      Object.keys(log).forEach((name) => {
+        if (name == "info") {
+          toLog += "<b>" + log[name] + "</b>\n";
+        } else {
+          toLog += name + ": " + log[name] + "\n";
         }
-    } else {
-        Object.keys(log).forEach(name => {
-            toLog += name + ": " + log[name] + "\n";
-        });
-        toLog += "- - -";
-        client.sendNotice(db.logto,toLog);
-    }
-    console.log("= = = = = = = = = = = = = = = =");
-}
+      });
+      toLog += "\n- - -";
+      return client.sendMessage(db.logto, {
+        body: toLog.replace(/<b>|<\/b>/g, ""),
+        msgtype: "m.notice",
+        format: "org.matrix.custom.html",
+        formatted_body: toLog,
+      });    }
+  } else {
+    Object.keys(log).forEach((name) => {
+      if (name == "info") {
+        toLog += "<b>" + log[name] + "</b>\n";
+      } else {
+        toLog += name + ": " + log[name] + "\n";
+      }
+    });
+    toLog += "\n- - -";
+    return client.sendMessage(db.logto, {
+      body: toLog.replace(/<b>|<\/b>/g, ""),
+      msgtype: "m.notice",
+      format: "org.matrix.custom.html",
+      formatted_body: toLog,
+    });
+  }
+  console.log("= = = = = = = = = = = = = = = =");
+};
